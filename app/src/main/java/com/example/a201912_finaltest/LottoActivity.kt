e2ea952832139b486429b2d832b22c0f1e2a5177
hyun <rlaalgus132@naver.com>
2019-12-29 10:29:44 +0900

    로또 화면 초안까지 작성

---
+++ app/src/main/java/com/example/a201912_finaltest/LottoActivity.kt
@@ -0,0 +1,23 @@
+package com.example.a201912_finaltest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LottoActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lotto)
+
+        setValues()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        }
+
+}
