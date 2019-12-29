3ddb34f3ec6992c349734a375de47999b03cbefb
hyun <rlaalgus132@naver.com>
2019-12-29 11:08:59 +0900

    당첨번호 뽑는 기능 마무리   - 화면에 반영할 차례

--- app/src/main/java/com/example/a201912_finaltest/LottoActivity.kt
+++ app/src/main/java/com/example/a201912_finaltest/LottoActivity.kt
@@ -2,6 +2,7 @@ package com.example.a201912_finaltest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_lotto.*
 import java.util.*
 import kotlin.collections.ArrayList
@@ -28,12 +29,13 @@ class LottoActivity : BaseActivity() {
     }
 
     fun makeWinLottoNum(){
+//        기존의 당첨번호 모두 삭제
 //        6개의 당첨 번호 생성 => 6번 반복
 //        랜덤으로 숫자를 생성=> 아무 제약 없는 랜덤이 아니라 => 1~45 의 범위./ 중복 허용 x 제약조건.
 //        제약조건을 통과한다면 => 당첨번호 목록으로 추가. => 배열(ArrayList)을 사용.
 //        작은 숫자부터 나타나도록 정렬
 //        여기까지 완료되면 6개의 텍스트뷰에 반영.
-
+        winLottoNumArr.clear()
         for (i in 0..5){
             while (true) {
                 val randomInt = Random().nextInt(45) + 1  // 0~44의 랜덤값 + 1 => 1~45의 랜덤
@@ -52,9 +54,13 @@ class LottoActivity : BaseActivity() {
                     winLottoNumArr.add(randomInt)
                     break
                 }
-
-                winLottoNumArr.add(randomInt)
             }
+        }
+//        Collections 클래스의 기능을 이용해서 ArrayList내부의 값을 정렬.
+        Collections.sort(winLottoNumArr)
+
+        for (num in winLottoNumArr){
+            Log.d("당첨번호:","${num}")
         }
 
     }
